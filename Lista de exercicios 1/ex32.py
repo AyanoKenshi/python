f8e9bbfdd600fdcf0d1f2f0a4d9810369a7464d3
192el <samuelmagoncalves@gmail.com>
2022-10-07 19:25:02 -0300

    pq?

--- Lista de exercicios 1/ex32.py
+++ Lista de exercicios 1/ex32.py
@@ -1,8 +1,10 @@
-import random                                                               #é serio esse import random é importante
+import random
+from turtle import clear                                                               #é serio esse import random é importante
 a = random.randint(1,5)
 jogada = int(input("tente adivinhar o numero entre 1 e 5: "))
 if a == jogada:
     print(f'a maquina jogou {a}, você acertou!')
 else:
     print(f'a maquina jogou {a}, você errou :(')        #bem mais simples que o outro
-    
+import os
+os.system('cls')
